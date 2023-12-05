a995ace3cef58b2e8fc960f4e1f9d38eb5e4eec6
Keunhong Park <github@void.im>
2023-12-04 17:05:28 -0800

    Videuler Responsiverbin

--- js/synced_video_selector.js
+++ js/synced_video_selector.js
@@ -2,47 +2,59 @@
 // http://thenewcode.com/364/Interactive-Before-and-After-Video-Comparison-in-HTML5-Canvas
 // with additional modifications based on: https://jsfiddle.net/7sk5k4gp/13/
 
-function playVids(videoId, targetHeight=2000) {
-    var videoMerge = document.getElementById(videoId + "Merge");
-    var slider = document.getElementById(videoId + "Slider");
-    var vid = document.getElementById(videoId);
+function playVids(videoId, container, targetHeight = 2000) {
+  var videoMerge = document.getElementById(videoId + "Merge");
+  var slider = document.getElementById(videoId + "Slider");
+  var vid = document.getElementById(videoId);
 
-    var vidWidthOrig = vid.videoWidth;
-    var vidHeightOrig = vid.videoHeight;
-    var vidWidth = targetHeight * vidWidthOrig / vidHeightOrig;
-    var vidHeight = targetHeight/7;
-    
-    var mergeContext = videoMerge.getContext("2d");
-    if (vid.readyState > 3) {
-        vid.play();
-        
-        function drawLoop() {
-            mergeContext.drawImage(vid, 0, slider.value*vidHeightOrig/7, vidWidthOrig, vidHeightOrig/7, 0, 0, vidWidth, vidHeight);
-            requestAnimationFrame(drawLoop);
 
-                    
-        }
-        requestAnimationFrame(drawLoop);
-    } 
-}        
+  var mergeContext = videoMerge.getContext("2d");
+  if (vid.readyState > 3) {
+    vid.play();
 
-Number.prototype.clamp = function(min, max) {
+    function drawLoop() {
+      var vidWidthOrig = vid.videoWidth;
+      var vidHeightOrig = vid.videoHeight;
+      var vidWidth = container.width();
+      var vidHeight = vidWidth * vidHeightOrig / vidWidthOrig / 7;
+
+      mergeContext.drawImage(vid, 0, slider.value * vidHeightOrig / 7, vidWidthOrig, vidHeightOrig / 7, 0, 0, vidWidth, vidHeight);
+      requestAnimationFrame(drawLoop);
+
+
+    }
+    requestAnimationFrame(drawLoop);
+  }
+}
+
+Number.prototype.clamp = function (min, max) {
   return Math.min(Math.max(this, min), max);
 };
-    
-    
-function resizeAndPlay(element, targetHeight=2000)
-{
-  var cv = document.getElementById(element.id + "Merge");
-  var slider = document.getElementById(element.id + "Slider");
+
+
+function resizeAndPlay(videoElement, targetHeight = 2000) {
+  var canvas = document.getElementById(videoElement.id + "Merge");
+  var slider = document.getElementById(videoElement.id + "Slider");
   const sliderValue = slider.value;
-    
+  const container = $('.video-compare-container');
+
   console.log(sliderValue)
- 
-  cv.width = targetHeight / element.videoHeight * element.videoWidth;
-  cv.height = targetHeight/7;
-  element.play();
-  element.style.height = "0px";  // Hide video without stopping it
-    
-  playVids(element.id, targetHeight);
+
+  canvas.width = targetHeight / videoElement.videoHeight * videoElement.videoWidth;
+  canvas.height = targetHeight / 7;
+  videoElement.play();
+  videoElement.style.height = "0px";  // Hide video without stopping it
+
+  playVids(videoElement.id, container, targetHeight);
+
+  console.log(canvas);
+
+  $(window).on('resize', function (e) {
+    const videoWidth = videoElement.videoWidth;
+    const videoHeight = videoElement.videoHeight / 7;
+    const canvasWidth = container.width();
+    const canvasHeight = canvasWidth * videoHeight / videoWidth;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+  });
 }
--- js/video_comparison.js
+++ js/video_comparison.js
@@ -1,130 +1,183 @@
 // Written by Dor Verbin, October 2021
 // This is based on: http://thenewcode.com/364/Interactive-Before-and-After-Video-Comparison-in-HTML5-Canvas
 // With additional modifications based on: https://jsfiddle.net/7sk5k4gp/13/
-
-function playVids(videoId, targetHeight=420) {
-    var videoMerge = document.getElementById(videoId + "Merge");
-    var vid = document.getElementById(videoId);
-
-    var position = 0.5;
-    // var vidWidth = vid.videoWidth/2;
-    // var vidHeight = vid.videoHeight;
-    var vidWidthOrig = vid.videoWidth/2;
-    var vidHeightOrig = vid.videoHeight;
-    var vidWidth = targetHeight / vid.videoHeight * vid.videoWidth/2;
-    var vidHeight = targetHeight;
-
-    var mergeContext = videoMerge.getContext("2d");
-
-    
-    if (vid.readyState > 3) {
-        vid.play();
-
-        function trackLocation(e) {
-            // Normalize to [0, 1]
-            bcr = videoMerge.getBoundingClientRect();
-            position = ((e.pageX - bcr.x) / bcr.width);
-        }
-        function trackLocationTouch(e) {
-            // Normalize to [0, 1]
-            bcr = videoMerge.getBoundingClientRect();
-            position = ((e.touches[0].pageX - bcr.x) / bcr.width);
-        }
-
-        videoMerge.addEventListener("mousemove",  trackLocation, false); 
-        videoMerge.addEventListener("touchstart", trackLocationTouch, false);
-        videoMerge.addEventListener("touchmove",  trackLocationTouch, false);
+// Modified by Keunhong Park to be responsive to window size.
 
 
-        function drawLoop() {
-            mergeContext.drawImage(vid, 0, 0, vidWidthOrig, vidHeightOrig, 0, 0, vidWidth, vidHeight);
-            var colStartOrig = (vidWidthOrig * position).clamp(0.0, vidWidthOrig);
-            var colWidthOrig = (vidWidthOrig - (vidWidthOrig * position)).clamp(0.0, vidWidthOrig);
-            var colStart = (vidWidth * position).clamp(0.0, vidWidth);
-            var colWidth = (vidWidth - (vidWidth * position)).clamp(0.0, vidWidth);
-            mergeContext.drawImage(vid, colStartOrig+vidWidthOrig, 0, colWidthOrig, vidHeightOrig, colStart, 0, colWidth, vidHeight);
-            requestAnimationFrame(drawLoop);
-
-            
-            var arrowLength = 0.09 * vidHeight;
-            var arrowheadWidth = 0.025 * vidHeight;
-            var arrowheadLength = 0.04 * vidHeight;
-            var arrowPosY = vidHeight / 10;
-            var arrowWidth = 0.007 * vidHeight;
-            var currX = vidWidth * position;
-
-            // // Draw circle
-            // mergeContext.arc(currX, arrowPosY, arrowLength*0.7, 0, Math.PI * 2, false);
-            // mergeContext.fillStyle = "#FFD79340";
-            // mergeContext.fill()
-            //mergeContext.strokeStyle = "#444444";
-            //mergeContext.stroke()
-            
-            // Draw border
-            mergeContext.beginPath();
-            mergeContext.moveTo(vidWidth*position, 0);
-            mergeContext.lineTo(vidWidth*position, vidHeight);
-            mergeContext.closePath()
-            mergeContext.strokeStyle = "#AAAAAA";
-            mergeContext.lineWidth = 5;            
-            mergeContext.stroke();
-
-            // Draw arrow
-            mergeContext.beginPath();
-            mergeContext.moveTo(currX, arrowPosY - arrowWidth/2);
-            
-            // Move right until meeting arrow head
-            mergeContext.lineTo(currX + arrowLength/2 - arrowheadLength/2, arrowPosY - arrowWidth/2);
-            
-            // Draw right arrow head
-            mergeContext.lineTo(currX + arrowLength/2 - arrowheadLength/2, arrowPosY - arrowheadWidth/2);
-            mergeContext.lineTo(currX + arrowLength/2, arrowPosY);
-            mergeContext.lineTo(currX + arrowLength/2 - arrowheadLength/2, arrowPosY + arrowheadWidth/2);
-            mergeContext.lineTo(currX + arrowLength/2 - arrowheadLength/2, arrowPosY + arrowWidth/2);
-
-            // Go back to the left until meeting left arrow head
-            mergeContext.lineTo(currX - arrowLength/2 + arrowheadLength/2, arrowPosY + arrowWidth/2);
-            
-            // Draw left arrow head
-            mergeContext.lineTo(currX - arrowLength/2 + arrowheadLength/2, arrowPosY + arrowheadWidth/2);
-            mergeContext.lineTo(currX - arrowLength/2, arrowPosY);
-            mergeContext.lineTo(currX - arrowLength/2 + arrowheadLength/2, arrowPosY  - arrowheadWidth/2);
-            mergeContext.lineTo(currX - arrowLength/2 + arrowheadLength/2, arrowPosY);
-            
-            mergeContext.lineTo(currX - arrowLength/2 + arrowheadLength/2, arrowPosY - arrowWidth/2);
-            mergeContext.lineTo(currX, arrowPosY - arrowWidth/2);
-
-            mergeContext.closePath();
-
-            mergeContext.fillStyle = "#AAAAAA";
-            mergeContext.fill();
-
-            // Draw circle
-            mergeContext.arc(currX, arrowPosY, arrowLength*0.7, 0, Math.PI * 2, false);
-            mergeContext.fillStyle = "#FFD79340";
-            mergeContext.fill()
-            
-            
-        }
-        requestAnimationFrame(drawLoop);
-    } 
-}
-
-Number.prototype.clamp = function(min, max) {
+Number.prototype.clamp = function (min, max) {
   return Math.min(Math.max(this, min), max);
 };
-    
-    
-function resizeAndPlay(element, targetHeight=420)
-{
-  var cv = document.getElementById(element.id + "Merge");
-//   cv.width = element.videoWidth/2;
-//   cv.height = element.videoHeight;
-  
-  cv.width = targetHeight / element.videoHeight * element.videoWidth/2;
-  cv.height = targetHeight;
-  element.play();
-  element.style.height = "0px";  // Hide video without stopping it
-    
-  playVids(element.id, targetHeight);
+
+
+class VideoComparison {
+  constructor(container) {
+      this.container = container;
+      this.position = 0.5;
+      this.canvas = container.find('canvas');
+      this.video = container.find('video');
+      this.context = this.canvas[0].getContext("2d");
+
+      this.isPlaying = false;
+
+      this.video[0].style.height = "0px";  // Hide video without stopping it
+      this.video[0].playbackRate = 0.5;
+
+      let self = this;
+      container.on('tab:show', function (e) {
+          self.playWhenReady();
+      });
+      container.on('tab:hide', function(e) {
+          // self.video[0].pause();
+          self.pause();
+      });
+
+      function trackLocation(e) {
+          // Normalize to [0, 1]
+          self.bcr = self.canvas[0].getBoundingClientRect();
+          self.position = ((e.pageX - self.bcr.x) / self.bcr.width);
+      }
+      function trackLocationTouch(e) {
+          // Normalize to [0, 1]
+          self.bcr = self.canvas[0].getBoundingClientRect();
+          self.position = ((e.touches[0].pageX - self.bcr.x) / self.bcr.width);
+      }
+
+      this.canvas.on('mousemove', trackLocation);
+      this.canvas.on('touchstart', trackLocationTouch);
+      this.canvas.on('touchmove', trackLocationTouch);
+      this.canvas.on('mouseout', function () { self.position = 0.5; });
+
+      $(window).on('resize', function (e) {
+          self.resize();
+      });
+  }
+
+  resize() {
+      const videoWidth = this.video[0].videoWidth / 2;
+      const videoHeight = this.video[0].videoHeight;
+      const canvasWidth = this.container.width();
+      const canvasHeight = canvasWidth * videoHeight / videoWidth;
+      this.canvas[0].width = canvasWidth;
+      this.canvas[0].height = canvasHeight;
+  }
+
+  play() {
+      this.resize();
+      if (this.isPlaying) {
+          return;
+      }
+      console.log('Playing video', this.video[0])
+      this.isPlaying = true;
+      this.video[0].play();
+      this.drawLoop();
+  }
+
+  pause() {
+      this.video[0].pause();
+      this.isPlaying = false;
+  }
+
+  playWhenReady() {
+      console.log('play when ready', this.video[0])
+      const self = this;
+      if (self.video[0].readyState >= 3) {
+          self.play();
+      } else if (!self.readyStateListenerAttached) {
+          document.addEventListener('readystatechange', function () {
+              if (self.video[0].readyState >= 3) {
+                  self.play();
+              }
+          });
+      }
+  }
+
+  drawLoop() {
+      const self = this;
+      const video = this.video[0];
+      const container = this.container;
+      const context = this.context;
+      requestAnimationFrame(drawFrame);
+
+      function drawFrame() {
+          const videoWidth = video.videoWidth / 2;
+          const videoHeight = video.videoHeight;
+          const canvasWidth = container.width();
+          const canvasHeight = canvasWidth * videoHeight / videoWidth;
+          const position = self.position;
+
+          context.drawImage(video, 0, 0, videoWidth, videoHeight, 0, 0, canvasWidth, canvasHeight);
+          var colStart = (canvasWidth * position).clamp(0.0, canvasWidth);
+          var colWidth = (canvasWidth - (canvasWidth * position)).clamp(0.0, canvasWidth);
+          var sourceColStart = (videoWidth * position).clamp(0.0, videoWidth);
+          var sourceColWidth = (videoWidth - (videoWidth * position)).clamp(0.0, videoWidth);
+          context.drawImage(
+              video,
+              sourceColStart + videoWidth, 0,
+              sourceColWidth, videoHeight,
+              colStart, 0,
+              colWidth, canvasHeight);
+
+          var arrowLength = 0.09 * canvasHeight;
+          var arrowheadWidth = 0.025 * canvasHeight;
+          var arrowheadLength = 0.04 * canvasHeight;
+          var arrowPosY = canvasHeight / 10;
+          var arrowWidth = 0.007 * canvasHeight;
+          var currX = canvasWidth * position;
+
+          // Draw circle
+          context.arc(currX, arrowPosY, arrowLength * 0.7, 0, Math.PI * 2, false);
+          context.fillStyle = "#FFD79340";
+          context.fill()
+
+          // Draw border
+          context.beginPath();
+          context.moveTo(canvasWidth * position, 0);
+          context.lineTo(canvasWidth * position, canvasHeight);
+          context.closePath()
+          context.strokeStyle = "#AAAAAA";
+          context.lineWidth = 5;
+          context.stroke();
+
+          // Draw arrow
+          context.beginPath();
+          context.moveTo(currX, arrowPosY - arrowWidth / 2);
+
+          // Move right until meeting arrow head
+          context.lineTo(currX + arrowLength / 2 - arrowheadLength / 2, arrowPosY - arrowWidth / 2);
+
+          // Draw right arrow head
+          context.lineTo(currX + arrowLength / 2 - arrowheadLength / 2, arrowPosY - arrowheadWidth / 2);
+          context.lineTo(currX + arrowLength / 2, arrowPosY);
+          context.lineTo(currX + arrowLength / 2 - arrowheadLength / 2, arrowPosY + arrowheadWidth / 2);
+          context.lineTo(currX + arrowLength / 2 - arrowheadLength / 2, arrowPosY + arrowWidth / 2);
+
+          // Go back to the left until meeting left arrow head
+          context.lineTo(currX - arrowLength / 2 + arrowheadLength / 2, arrowPosY + arrowWidth / 2);
+
+          // Draw left arrow head
+          context.lineTo(currX - arrowLength / 2 + arrowheadLength / 2, arrowPosY + arrowheadWidth / 2);
+          context.lineTo(currX - arrowLength / 2, arrowPosY);
+          context.lineTo(currX - arrowLength / 2 + arrowheadLength / 2, arrowPosY - arrowheadWidth / 2);
+          context.lineTo(currX - arrowLength / 2 + arrowheadLength / 2, arrowPosY);
+
+          context.lineTo(currX - arrowLength / 2 + arrowheadLength / 2, arrowPosY - arrowWidth / 2);
+          context.lineTo(currX, arrowPosY - arrowWidth / 2);
+
+          context.closePath();
+
+          context.fillStyle = "#AAAAAA";
+          context.fill();
+
+          context.font = "20px 'Google Sans', sans-serif";
+          context.fillStyle = "white";
+          context.strokeStyle = 'black';
+          context.lineWidth = 2;
+          context.textAlign = "left";
+          context.textBaseline = "bottom";
+
+          if (self.isPlaying) {
+              requestAnimationFrame(drawFrame);
+          }
+      }
+  }
 }
