e63e58f68986051161302be4c7464818cc05fc37
Keunhong Park <github@void.im>
2023-12-05 01:27:39 -0800

    Improve scene buttons.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+// JavaScript to handle mouseover and mouseout events
+var activeMethodPill = null;
+var activeScenePill = null;
+var activeModePill = null;
+var activeVidID = 0;
+var select = false;
+
 
 $(document).ready(function() {
     var editor = CodeMirror.fromTextArea(document.getElementById("bibtex"), {
@@ -9,6 +16,9 @@ $(document).ready(function() {
         $('[data-toggle="tooltip"]').tooltip()
     });
     
+activeMethodPill = $('.method-pill').filter('.active')[0];
+activeModePill = $('.mode-pill').filter('.active')[0];
+activeScenePill = $('.scene-pill').filter('.active')[0];
 
 // var frameNumber = 0, // start video at frame 0
 //     // lower numbers = faster playback
@@ -38,3 +48,65 @@ $(document).ready(function() {
     
 //     window.requestAnimationFrame(scrollPlay);
 });
+
+function selectCompVideo(methodPill, scenePill, n_views, modePill) {
+    // Your existing logic for video selection
+    // var video = document.getElementById("compVideo");
+    select = true;
+    var videoSwitch = document.getElementById("compVideoSwitch");
+    var viewNum = document.getElementById("compVideoValue");
+
+    $('.method-pill').each(function(e){
+        if ($(this).hasClass("active")) {
+            activeMethodPill = this;
+        };
+    })
+
+    $('.scene-pill').each(function(e){
+        if ($(this).hasClass("active")) {
+            activeScenePill = this;
+        };
+    })
+
+    $('.mode-pill').each(function(e){
+        if ($(this).hasClass("active")) {
+            activeScenePill = this;
+        };
+    })
+
+    if (activeMethodPill) {
+        activeMethodPill.classList.remove("active");
+    }
+    if (activeScenePill) {
+        activeScenePill.classList.remove("active");
+    }
+    if (modePill) {
+        activeModePill.classList.remove("active");
+        modePill.classList.add("active");
+        activeModePill = modePill;
+    }
+    activeMethodPill = methodPill;
+    activeScenePill = scenePill;
+    methodPill.classList.add("active");
+    scenePill.classList.add("active");
+    method = methodPill.getAttribute("data-value");
+    pill = scenePill.getAttribute("data-value");
+    mode = activeModePill.getAttribute("data-value");
+
+    // if (videoSwitch.checked) {
+    //     mode = 'depth'
+    // } else {
+    //     mode = 'rgb'
+    // }
+
+    // swap video to avoid flickering
+    activeVidID = 1 - activeVidID;
+    var video_active = document.getElementById("compVideo" + activeVidID);
+    var video_hidden = document.getElementById("compVideo" + (1 - activeVidID));
+    video_active.src = "videos/comparison/" + pill + "_" + method + "_vs_ours_" + mode + ".mp4";
+    video_active.load();
+
+    if (n_views) {
+        viewNum.innerHTML = n_views;
+    }
+}
